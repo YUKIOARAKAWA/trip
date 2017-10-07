79bb3bd9499727fbff449260147fe18d6a39201f
荒川幸生 <>
2017-10-07 03:32:57 +0000

    unicorn設定ファイル、psqlからmysqlに変更

--- db/schema.rb
+++ db/schema.rb
@@ -13,43 +13,40 @@
 
 ActiveRecord::Schema.define(version: 20160829090928) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "areas", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "blogs", force: :cascade do |t|
-    t.string   "Test"
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "Test",       limit: 255
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "pins", force: :cascade do |t|
-    t.integer  "place_id"
-    t.integer  "user_id"
-    t.string   "comment"
-    t.integer  "want"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "place_id",   limit: 4
+    t.integer  "user_id",    limit: 4
+    t.string   "comment",    limit: 255
+    t.integer  "want",       limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_index "pins", ["place_id"], name: "index_pins_on_place_id", using: :btree
   add_index "pins", ["user_id"], name: "index_pins_on_user_id", using: :btree
 
   create_table "places", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "plan_id"
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.integer  "route"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "user_id",    limit: 4
+    t.integer  "plan_id",    limit: 4
+    t.string   "address",    limit: 255
+    t.float    "latitude",   limit: 24
+    t.float    "longitude",  limit: 24
+    t.integer  "route",      limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.datetime "from"
     t.datetime "to"
   end
@@ -58,42 +55,42 @@ ActiveRecord::Schema.define(version: 20160829090928) do
   add_index "places", ["user_id"], name: "index_places_on_user_id", using: :btree
 
   create_table "plan_users", force: :cascade do |t|
-    t.integer  "plan_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "plan_id",    limit: 4
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   add_index "plan_users", ["plan_id"], name: "index_plan_users_on_plan_id", using: :btree
   add_index "plan_users", ["user_id"], name: "index_plan_users_on_user_id", using: :btree
 
   create_table "plans", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",       limit: 255
     t.date     "start_date"
     t.date     "end_date"
-    t.integer  "area_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "area_id",    limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_index "plans", ["area_id"], name: "index_plans_on_area_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "uid"
-    t.string   "provider"
-    t.string   "name"
+    t.integer  "current_sign_in_ip",     limit: 4
+    t.integer  "last_sign_in_ip",        limit: 4
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "uid",                    limit: 255
+    t.string   "provider",               limit: 255
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
